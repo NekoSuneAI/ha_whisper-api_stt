213550d0ea3f11285f0c43aed605d5bdb0a04341
NekoSuneVR <Chisdeal2014@hotmail.com>
2024-11-11 13:20:44 +0000

    Update __init__.py

--- custom_components/whisper_api_stt/__init__.py
+++ custom_components/whisper_api_stt/__init__.py
@@ -1,1 +1,19 @@
 """Custom integration for OpenAI Whisper API STT."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Whisper STT component."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Whisper STT from a config entry."""
+    hass.data[DOMAIN] = entry.data
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    hass.data.pop(DOMAIN)
+    return True
